5ec4a1a687638cceae4a60951701945266484347
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-13 16:33:40 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DealershipApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
