ceedc26fe4d9aae368c136582a4485692e040521
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-13 19:38:07 -0700

    Created a vehicle DB class

---
+++ VehicleDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealershipApp
+{
+    static class VehicleDB
+    {
+        /// <summary>
+        /// Retrieve all the cars that are in stock from the db
+        /// </summary>
+        /// <returns></returns>
+        public static List<String> GetAllInStockVehicles()
+        {
+
+        }
+    }
+}
